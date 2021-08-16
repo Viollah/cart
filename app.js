c0de08aeb31babc50f60567b595515a683c5d142
Viollah <Viollah0119@gmail.com>
2021-08-16 15:32:20 +0100

    sixth commit

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+  
 const smallPlusBtn = document.querySelector(".plusBtn.small");
 const mediumPlusBtn = document.querySelector(".plusBtn.medium");
 const largePlusBtn = document.querySelector(".plusBtn.large");
@@ -25,6 +26,16 @@ const payOut = document.querySelector(".payOut");
 const message = document.querySelector(".message");
 const payAmt = document.querySelector(".payAmt");
 const payBtn = document.querySelector(".payBtn");
+
+const containerBox = document.querySelector(".containerBox");
+const closeBtn = document.querySelector(".close");
+
+var width = screen.availWidth;
+var height = screen.availHeight;
+
+containerBox.style.width = width + 'px';
+containerBox.style.height = height + 'px';
+containerBox.style.paddingTop = height*1/3 + 'px';
 
 var smallQuantity = 0;
 var mediumQuantity = 0;
@@ -71,25 +82,89 @@ function BtnClick() {
     largePizzaTotal.innerHTML = (largeQuantity * 129).toFixed(2);
     totalCart = smallQuantity * 49.00 + mediumQuantity * 89.00 + largeQuantity * 129.00;
     cartTotal.innerHTML = totalCart.toFixed(2);
+
+    if (totalCart > 0) {
+        checkOut.classList.remove('hidden');
+    } else {
+        checkOut.classList.add('hidden');
+        payOut.classList.add('hidden');
+    }
 }
+
 
 function checkOutClick(){
     checkOut.classList.add('hidden');
     payOut.classList.remove('hidden');
 }
 
-function payment(){
-    message.classList.toggle('hidden');
+function payment() {
+    containerBox.classList.remove('hidden');
     var paymentAmt = Number(payAmt.value);
- if (paymentAmt == totalCart){
-    message.innerHTML = "Thank you, here's your pizza!";
- } else if (paymentAmt > totalCart) {
-    var change = paymentAmt - totalCart;
-    message.innerHTML = "Have a good day, take your change R" + change.toFixed(2);
- } else {
-    message.innerHTML = "Sorry, insuffient!";
- }
+    if (paymentAmt == totalCart) {
+        containerBox.style.backgroundColor = 'rgba(120, 255, 120, 0.95)';
+        message.innerHTML = "Enjoy your Pizza!";
+        smallQuantity = 0;
+        mediumQuantity= 0;
+        largeQuantity= 0;
+        totalCart = 0;
+
+        smallPizzaQuantity.innerHTML = smallQuantity;
+        mediumPizzaQuantity.innerHTML = mediumQuantity;
+        largePizzaQuantity.innerHTML = largeQuantity;
+
+        smallPizzaTotal.innerHTML = (smallQuantity * 49).toFixed(2);
+        mediumPizzaTotal.innerHTML = (mediumQuantity* 89).toFixed(2);
+        largePizzaTotal.innerHTML = (largeQuantity * 129).toFixed(2);
+        totalCart = smallQuantity * 49.00 + mediumQuantity * 89.00 + largeQuantity * 129.00;
+        cartTotal.innerHTML = totalCart.toFixed(2);
+
+        setTimeout(function () {
+            containerBox.classList.add('hidden');
+            checkOut.classList.remove('hidden');
+            payOut.classList.add('hidden');
+            payAmt.value = "";
+        }, 4500);
+
+    } else if (paymentAmt > totalCart) {
+        var change = paymentAmt - totalCart;
+        containerBox.style.backgroundColor = 'rgba(120, 255, 120, 0.95)';
+        message.innerHTML = "Enjoy your Pizza, here is your change R" + change.toFixed(2);
+        smallQuantity = 0;
+        mediumQuantity = 0;
+        largeQuantity = 0;
+        totalCart = 0;
+
+        smallPizzaQuantity.innerHTML = smallQuantity;
+        mediumPizzaQuantity.innerHTML = mediumQuantity;
+        largePizzaQuantity.innerHTML = largeQuantity;
+
+        smallPizzaTotal.innerHTML = (smallQuantity * 49).toFixed(2);
+        mediumPizzaTotal.innerHTML = (mediumQuantity * 89).toFixed(2);
+        largePizzaTotal.innerHTML = (largeQuantity * 129).toFixed(2);
+        totalCart = smallQuantity * 49.00 + mediumQuantity * 89.00 + largeQuantity * 129.00;
+        cartTotal.innerHTML = totalCart.toFixed(2);
+
+        setTimeout(function () {
+            containerBox.classList.add('hidden');
+            checkOut.classList.remove('hidden');
+            payOut.classList.add('hidden');
+            payAmt.value = "";
+        }, 4500);
+
+    } else {
+        containerBox.style.backgroundColor = 'rgba(255, 120, 120, 0.95)';
+        message.innerHTML = "Sorry, insuffient!";
+        setTimeout(function () {
+            containerBox.classList.add('hidden');
+        }, 4500);
+    }
 }
+
+function close() {
+    containerBox.classList.add('hidden');
+}
+
+
 
 smallPlusBtn.addEventListener('click', BtnClick);
 smallMinusBtn.addEventListener('click', BtnClick);
